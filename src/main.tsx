1f04d74fb52f82a26871a30c77baee5458bc98a5
woweeclapper <joenguyener4579@gmail.com>
2026-01-01 13:37:05 -0600

    WIP

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+//Make the UI BETTER
+//Check the logic performance so it render the data correctly
+//Make an empty "admin" page to let the visitor to test the site
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
